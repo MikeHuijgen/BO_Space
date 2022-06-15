20581d362257d16745464515a81977e47ddb8c06
j5x <32881@ma-web.nl>
2022-06-15 17:41:40 +0200

    half merg on patrolling and follow player

--- Assets/Scripts/Monster/FollowPlayer.cs
+++ Assets/Scripts/Monster/FollowPlayer.cs
@@ -8,16 +8,22 @@ public class FollowPlayer : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform player;
     [SerializeField] private GameObject flashLight;
+    
+    
+    public patrolling patrolling;
+    public DoorScript doorScript;
+    
+    
 
     private bool lightIsOn;
-    private Vector3 startPos;
+    
 
-    private NavMeshAgent agent;
+    public NavMeshAgent agent;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        startPos = transform.position;
+        
     }
 
     private void Update()
@@ -34,6 +40,14 @@ public class FollowPlayer : MonoBehaviour
         if (lightIsOn == true)
         {
             agent.SetDestination(player.position);
+        }
+        if (doorScript.monsterCanOpen == true)
+        {
+            patrolling.destPoint = (int)Mathf.Floor(Random.Range(0, 7)); }
+        else
+        {
+            //Need to change with the waypoint path
+            patrolling.destPoint = (int)Mathf.Floor(Random.Range(0, 5));
         }
     }
 }
